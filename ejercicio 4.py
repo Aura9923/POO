f1c085f9bce77cfd05d6e60de461de93df26f2bc
Aura9923 <aarangou@unal.edu.co>
2026-02-15 23:17:27 -0500

    ejercicio 4

--- ejercicio 4.py
+++ ejercicio 4.py
@@ -1,17 +1,14 @@
 class Edades:
-
     def edad_alberto(edad_juan):
         return 2 * edad_juan / 3
 
     def edad_ana(edad_juan):
-        return 4 * edad_juan / 3
+        return 4 * edad_juan /3
 
     def edad_mama(edad_juan, edad_alberto, edad_ana):
         return edad_juan + edad_alberto + edad_ana
 
-
 edad_juan = 9
-
 edad_alberto = Edades.edad_alberto(edad_juan)
 edad_ana = Edades.edad_ana(edad_juan)
 edad_mama = Edades.edad_mama(edad_juan, edad_alberto, edad_ana)
